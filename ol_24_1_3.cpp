447423ee2bb71912d4ceddcc48d116c3574f2d32
Mitia-j <dimazmud@gmail.com>
2025-03-08 22:44:41 +0200

    dsfjvk

--- ol_24_1_3.cpp
+++ ol_24_1_3.cpp
@@ -3,17 +3,6 @@
 using namespace std;
 
 int main() {
-    cin >> N >> M;
-    matrix.resize(N, vector<int>(M));
-
-    for (int i = 0; i < N; i++) {
-        string row;
-        cin >> row;
-        for (int j = 0; j < M; j++) {
-            matrix[i][j] = row[j] - '0';
-        }
-    }
-
-    cout << findMaxBlocks() << endl;
+    cout<<"LOH";     
     return 0;
 }
