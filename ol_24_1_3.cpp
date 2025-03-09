6ca11d8750e504f5309a8ccceb2f6774805d284f
Mitia-j <dimazmud@gmail.com>
2025-03-09 22:56:00 +0200

    skelet

--- ol_24_1_3.cpp
+++ ol_24_1_3.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
-
 using namespace std;
-
+const int nmax = 300000;
+struct av {
+    int h, x;
+    unsigned d;
+};
+av a[nmax];
+int N, i;
 int main() {
-    cout<<"LOH";     
+    
+    for (i=0; i<N; i++){
+        cin>>a[i].h>>a[i].x>>a[i].d;
+    }
     return 0;
 }
