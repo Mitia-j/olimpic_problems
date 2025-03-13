93e3e85ba80d5f09f1c1311ff62b145d75758301
Mitia-j <dimazmud@gmail.com>
2025-03-13 21:18:14 +0200

    trying to fix this sh.

--- ol_24_1_3.cpp
+++ ol_24_1_3.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 using namespace std;
+int cnt, MaxCount, i, N;
 const int nmax = 300000;
 struct av {
     int h, x;
     unsigned d;
 };
 av a[nmax];
-int N, i;
 int main() {
-    
     for (i=0; i<N; i++){
         cin>>a[i].h>>a[i].x>>a[i].d;
+    }
+    while (true){
+        for (i=0; i<N; i++){
+            for (int j=-i; j<=i; j++){
+                if (a[i].x==j) cnt++;
+            }
+            if (a[i].d==0) a[i].x--;
+            else a[i].x++;
+        }
+        if (MaxCount<=cnt) MaxCount = cnt;
+        else break;
     }
     return 0;
 }
