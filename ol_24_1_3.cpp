e81341fbbf8c27dccb9e81fb739d421df7e509f4
Mitia-j <dimazmud@gmail.com>
2025-03-17 22:07:36 +0200

    First version

--- ol_24_1_3.cpp
+++ ol_24_1_3.cpp
@@ -1,26 +1,36 @@
 #include <iostream>
 using namespace std;
-int cnt, MaxCount, i, N;
 const int nmax = 300000;
 struct av {
     int h, x;
     unsigned d;
-};
-av a[nmax];
+} a[nmax];
+int cnt = 0, MaxCount = 0, i, N;
 int main() {
-    for (i=0; i<N; i++){
-        cin>>a[i].h>>a[i].x>>a[i].d;
+    cin >> N; // Читаем N перед использованием
+    if (N > nmax) {
+        cerr << "Ошибка: N превышает размер массива!" << endl;
+        return 1;
     }
-    while (true){
-        for (i=0; i<N; i++){
-            for (int j=-i; j<=i; j++){
-                if (a[i].x==j) cnt++;
+
+    for (i = 0; i < N; i++) {
+        cin >> a[i].h >> a[i].x >> a[i].d;
+    }
+    while (true) {
+        cnt = 0; // Обнуляем cnt перед подсчётом
+
+        for (i = 0; i < N; i++) {
+            for (int j = -i; j <= i; j++) { // Улучшенный диапазон
+                if (a[i].x == j) cnt++;
             }
-            if (a[i].d==0) a[i].x--;
+            if (a[i].d == 0) a[i].x--;
             else a[i].x++;
         }
-        if (MaxCount<=cnt) MaxCount = cnt;
+
+        if (MaxCount < cnt) MaxCount = cnt;
         else break;
     }
+
+    cout << "Максимальное количество: " << MaxCount << endl;
     return 0;
 }
