2e3fb2f163c05b99c8886706c88ac04a6e2cab99
Mitia-j <dimazmud@gmail.com>
2025-03-25 12:54:52 +0200

    pre-r ol_24_1_3

--- ol_24_1_3.cpp
+++ ol_24_1_3.cpp
@@ -5,7 +5,7 @@ struct av {
     int h, x;
     unsigned d;
 } a[nmax];
-int cnt = 0, MaxCount = 0, i, N;
+int cnt = 0, MaxCount = 0, i, N, i1=-1, i2=1, j, im=0;
 int main() {
     cin >> N; // Читаем N перед использованием
     if (N > nmax) {
@@ -16,21 +16,28 @@ int main() {
     for (i = 0; i < N; i++) {
         cin >> a[i].h >> a[i].x >> a[i].d;
     }
-    while (true) {
+    while (im<1000) {
         cnt = 0; // Обнуляем cnt перед подсчётом
 
         for (i = 0; i < N; i++) {
-            for (int j = -i; j <= i; j++) { // Улучшенный диапазон
-                if (a[i].x == j) cnt++;
+            for (j=0; j<nmax; j++){
+                if (a[i].x >= i1 && a[i].x <= i2)
+                    if(a[i].h >= i2){ 
+                        cnt++;
+                        goto next;
+                    }
+                    else goto next;
+                i1--; i2++;
             }
+        next:
             if (a[i].d == 0) a[i].x--;
             else a[i].x++;
+            i1=-1; i2=1;
         }
-
-        if (MaxCount < cnt) MaxCount = cnt;
-        else break;
+        if (MaxCount <= cnt) MaxCount = cnt;
+        //else break;
+        im++;
     }
-
     cout << "Максимальное количество: " << MaxCount << endl;
     return 0;
 }
