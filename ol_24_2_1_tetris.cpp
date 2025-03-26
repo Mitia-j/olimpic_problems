2e4fb0ccaebc472d9efaa9ca50a48677afda7252
Mitia-j <dimazmud@gmail.com>
2025-03-27 00:14:16 +0200

    frst 24 2 1 tetris

--- ol_24_2_1_tetris.cpp
+++ ol_24_2_1_tetris.cpp
@@ -6,9 +6,13 @@ int main (){
     int j=0, i=0, N, M;
     char masiv [nmax][nmax];
     cin>>N>>M;
-    
-        for (i=N-1; i>=0; i--)
-            for (j=M-1; j>=0; j--)
-                cin>>masiv[i][j];
+    for (i=0; i<N; i++)
+        for (j=0; j<M; j++)
+            cin>>masiv[i][j];
+    for (i=N-1; i>=0; i--)
+        for (j=M-1; j>=0; j--) {
+            if (masiv[i][j] == '.')
+                masiv[i][j] = masiv[i-1][j-1];
+        }
     return 0;
 }
